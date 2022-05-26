2d7976d4cf78b7a2c5f5d5a39bb60b68a8b4168c
lafx0 <lafx0.ip@gmail.com>
2022-05-26 23:15:12 +0200

    Create auto_configuration.py
    Auto configuration for tmux, vim and fish shell. Useful for fresh Linux installations.

---
+++ auto_configuration.py
@@ -0,0 +1,12 @@
+import os
+
+print("[* Configuring tmux ...]")
+os.system('wget https://raw.githubusercontent.com/lafx0/dotfiles/main/.tmux.conf -O ~/.tmux.conf')
+os.system("git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm")
+os.system("tmux source ~/.tmux.conf")
+print("[* tmux configured! Enjoy :)]")
+
+print("[* Now configuring vim ...")
+os.system("wget https://raw.githubusercontent.com/lafx0/dotfiles/main/.vimrc -O ~/.vimrc")
+os.system("source ~/.vimrc")
+print("vim configured! Enjoy :)")
